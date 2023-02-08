f6bd7c71aa56ab1238c22f64e81d0af34c49d2d3
Orifha1 <orifhatshikotshi1@gmail.com>
2023-02-08 20:28:27 +0200

    add user entity

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,24 +1,53 @@
-import { Field, ObjectType } from "type-graphql"
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm"
-//This is an example entity.
+import { Field, ID, ObjectType } from "type-graphql";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, CreateDateColumn } from "typeorm";
 
+/**
+ * This is the user entity.
+ */
 @ObjectType()
-@Entity()
+@Entity("user")
 export class User extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn("uuid")
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @Field()
+  @Column("text")
+  firstname: string;
 
-    @Field()
-    @Column()
-    firstName: string
+  @Field()
+  @Column("text")
+  lastname: string;
 
-    @Field()
-    @Column()
-    lastName: string
-    
-    @Field()
-    @Column()
-    age: number
+  @Field()
+  @Column()
+  user_name: string;
 
+  @Field()
+  @Column("text")
+  email: string;
+
+  @Field()
+  @Column("text")
+  unreliable: boolean = false;
+
+  @Field()
+  @Column("boolean", {default:false})
+  user_verified: boolean;
+
+  /**
+   * The password variable does not have the @Field() decorator because
+   * we do not want the password hash to be returned when we call our endpoint.
+   * So, this just makes it a column in the database.
+   */
+  @Column("text")
+  password: string;
+
+  @Field()
+  @CreateDateColumn()
+  creation_date: Date;
+
+  @Field()
+  @Column()
+  created_at: string;
 }
